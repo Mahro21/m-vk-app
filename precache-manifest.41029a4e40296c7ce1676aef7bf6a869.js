4b127038232a66a655d1d531673b555402db2654
Slava Korshunov <slavchik.musik@gmail.com>
2019-08-23 19:42:02 +0300

    Updates

---
+++ precache-manifest.41029a4e40296c7ce1676aef7bf6a869.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/Mahro21/m-vk-app.git/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "693f65f34c6c18909b65",
+    "url": "/Mahro21/m-vk-app.git/static/js/runtime~main.9fda5b38.js"
+  },
+  {
+    "revision": "e05e5a7176446822f27b",
+    "url": "/Mahro21/m-vk-app.git/static/js/main.276b5574.chunk.js"
+  },
+  {
+    "revision": "3b1d6c9f91f3da52b077",
+    "url": "/Mahro21/m-vk-app.git/static/js/2.28befc6e.chunk.js"
+  },
+  {
+    "revision": "e05e5a7176446822f27b",
+    "url": "/Mahro21/m-vk-app.git/static/css/main.0364cccb.chunk.css"
+  },
+  {
+    "revision": "3b1d6c9f91f3da52b077",
+    "url": "/Mahro21/m-vk-app.git/static/css/2.1394a3cc.chunk.css"
+  },
+  {
+    "revision": "8a031a0da58ba3e10d541eda398e4688",
+    "url": "/Mahro21/m-vk-app.git/index.html"
+  }
+];
