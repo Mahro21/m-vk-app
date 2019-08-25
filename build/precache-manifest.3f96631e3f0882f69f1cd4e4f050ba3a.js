b5cc952de2c006f8a4cc5f543c20be71bcab6495
Mahro21 <48554992+Mahro21@users.noreply.github.com>
2019-08-25 07:44:22 +0300

    Add files via upload

---
+++ build/precache-manifest.3f96631e3f0882f69f1cd4e4f050ba3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/m-vk-app/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "3af2085d2cf1e4cd2573",
+    "url": "/m-vk-app/static/js/runtime~main.007ab338.js"
+  },
+  {
+    "revision": "e05e5a7176446822f27b",
+    "url": "/m-vk-app/static/js/main.276b5574.chunk.js"
+  },
+  {
+    "revision": "3b1d6c9f91f3da52b077",
+    "url": "/m-vk-app/static/js/2.28befc6e.chunk.js"
+  },
+  {
+    "revision": "e05e5a7176446822f27b",
+    "url": "/m-vk-app/static/css/main.0364cccb.chunk.css"
+  },
+  {
+    "revision": "3b1d6c9f91f3da52b077",
+    "url": "/m-vk-app/static/css/2.1394a3cc.chunk.css"
+  },
+  {
+    "revision": "69534329c28ef70fe5dff00283b5c4ac",
+    "url": "/m-vk-app/index.html"
+  }
+];
